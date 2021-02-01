fa2f5f0f64b12d2701a4590ebe0c4ff454a9526e
4ions <leovalenciasantana@gmail.com>
2021-02-01 10:50:18 -0500

    Sorting algorithms

---
+++ 0-bubble_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ *swap - swap number in array
+ *@xp: number of array
+ *@yp: number of array
+ *
+ *Return: No return
+ */
+
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+/**
+ *bubble_sort - Function that sorts an array in ascending order
+ *@array: array to sort in ascending order
+ *@size: number of elements to sort
+ *
+ *Return: No return value
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;/** O(1)*/
+	int check = 0;/** O(1)*/
+
+	for (i = 0; i < size - 1; i++)/** O(n)*/
+	{
+		check = 0;/** O(n)*/
+		for (j = 0; j < size - i - 1; j++)/** O(n*n)*/
+		{
+			/** Check if next number is greater*/
+			if (array[j] > array[j + 1])/** O(n*n)*/
+			{
+				/** If number is greater, swap number*/
+				swap(&array[j], &array[j + 1]);/** O(n*n)*/
+				print_array(array, size);/** O(n*n)*/
+				/** "check"  keep the loop going*/
+				check = 1;/** O(n*n)*/
+			}
+		}
+		if (check == 0)/** O(n)*/
+			break;/** O(n)*/
+	}
+} /**  1 + 1 + n + n + n*n + n*n + n*n + n*n n*n + n +  n*/
+/** = 5n^2 + 3n + 2 = O(n^2) */
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,58 @@
+#include "sort.h"
+
+/**
+ *swap - swap nodes
+ *@prev: prev node
+ *@current: current node
+ *
+ *Return: no return
+ */
+void swap(listint_t *prev, listint_t *current)
+{
+	if (prev->prev)
+		prev->prev->next = current;
+	if (current->next)
+		current->next->prev = prev;
+	prev->next = current->next;
+	current->prev = prev->prev;
+	prev->prev = current;
+	current->next = prev;
+
+}
+
+/**
+ *insertion_sort_list - insert a node into list
+ *@list: list to insert node
+ *
+ *Return: no return
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *head, *current, *prev;
+
+	if (!list || !*list || !(*list)->next)/**0(1)*/
+		return;
+	head = (*list)->next;/**0(1)*/
+	while (head)/**0(n)*/
+	{
+		prev = current = head;/**0(n)*/
+		while (current && prev->prev)/**0(n^2)*/
+		{
+			if (prev->prev->n > current->n)/**0(n^2)*/
+			{
+				printf("%d\n", prev->prev->n);
+				swap(prev->prev, current);/**0(n^2)*/
+				if (!prev->prev)/**0(n^2)*/
+					*list = current;/**0(n^2)*/
+				print_list((const listint_t *)*list);/**0(n^2)*/
+
+			}
+			else/**0(n^2)*/
+			{
+				prev = prev->prev;/**0(n^2)*/
+			}
+
+		}
+		head = head->next;/**0(n)*/
+	}
+}
